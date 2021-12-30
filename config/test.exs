f3649e3dade068848a646a8e1ef5e0af104bd677
Vinicios <wentz.vinicios@gmail.com>
2021-12-30 10:53:15 -0300

    chore: add excoveralls to delivery best covegare reports

--- config/test.exs
+++ config/test.exs
@@ -10,6 +10,7 @@ config :rockelivery, Rockelivery.Repo,
   password: "postgres",
   database: "rockelivery_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
+  port: 5433,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
