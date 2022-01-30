53df584178a83d305ac3a757b68afda42b948a62
Vinicios <wentz.vinicios@gmail.com>
2022-01-30 17:38:46 -0300

    chore: test deps grouped

--- mix.exs
+++ mix.exs
@@ -52,10 +52,11 @@ defmodule Rockelivery.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:pbkdf2_elixir, "~> 1.4"},
-      {:excoveralls, "~> 0.10", only: :test},
-      {:ex_machina, "~> 2.7.0"},
       {:tesla, "~> 1.4"},
-      {:hackney, "~> 1.17"}
+      {:hackney, "~> 1.17"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:bypass, "~> 2.1", only: :test},
+      {:ex_machina, "~> 2.7.0", only: :test},
     ]
   end
 
